d2673addd9520dfc61b2dcd61939f08170b04b84
fisher521 <fisher521.fs@gmail.com>
2020-03-25 16:09:08 -0400

    feat(UserPanel): Add instructions

--- src/main/java/com/tactlessfish/connectfour/panels/UserPanel.java
+++ src/main/java/com/tactlessfish/connectfour/panels/UserPanel.java
@@ -124,7 +124,10 @@ public class UserPanel extends JPanel implements KeyListener, JavaArcade {
      */
     @Override
     public String getInstructions() {
-        return null;
+        return "Move using the arrow keys and drop a checker with space. " +
+                "\ntake turns dropping checkers until a player wins." +
+                "\n\nA player wins when they connect four of their pieces" +
+                "\nhorizontally, vertically, or diagonally.";
     }
 
     /**
